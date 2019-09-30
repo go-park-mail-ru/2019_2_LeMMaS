b8d64cf6b0a8b7fe6c7fafc87ddcb5436e33dffd
Ledka17 <ledkaconf@gmail.com>
2019-09-30 10:59:23 +0300

    something added

--- README.md
+++
@@ -1,9 +0,0 @@
-# 2019_2_LeMMaS
-
-Project "Agario"
-
-### Developers
-<a name="developers"></a>
-
-- [Sudarev Maxim](https://github.com/smi97)
-- [Mozhevikina Leonarda](https://github.com/ledka17)
---
+++ src/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+type ServerConfig struct {
+	Host      string `json:"host"`
+	PortURL   string `json:"portUrl"`
+	PortValue string `json:"portValue"`
+}
+
+type DatabaseConfig struct {
+	DriverName			string `json:"driverName"`
+	URL					string `json:"url"`
+	ConnectionString	string `json:"connectionString"`
+	MaxOpenConnections	int    `json:"maxOpenConnections"`
+}
+
+type SessionConfig struct {
+	Name            string		`json:"name"`
+	Value           string		`json:"length"`
+	LifetimeSeconds int    		`json:"lifetime"`
+	Secure			bool		`json:"secure"`
+	HTTPOnly        bool		`json:"httpOnly"`
+}
+
+type AuthConfig struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+	Email 	 string `json:"email"`
+}
