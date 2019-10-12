8d497b6ddf58e4e0a4f6e34cda4daaa3ec8247f1
Ledka17 <ledkaconf@gmail.com>
2019-10-13 01:05:26 +0300

    add tests handler

---
+++ handlers/middleware/middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func MethodMiddleware(method string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != method {
+				w.WriteHeader(405)
+				_, _ = w.Write([]byte("Method not allowed"))
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
