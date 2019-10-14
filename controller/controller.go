53f97a63e2dc5ec6c6352391ff97ff1f3c467bf5
anton <anton.cobzew@yandex.ru>
2019-10-14 15:57:17 +0300

    CORS middleware

--- controller/controller.go
+++ controller/controller.go
@@ -16,9 +16,6 @@ type APIResponse struct {
 
 func (c Controller) writeCommonHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func (c Controller) writeOk(w http.ResponseWriter) {
