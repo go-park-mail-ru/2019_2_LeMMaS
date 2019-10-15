256894e254a367b4b26a69f9e13244264b275012
anton <anton.cobzew@yandex.ru>
2019-10-15 14:38:45 +0300

    Change allowed origin

--- controller/api.go
+++ controller/api.go
@@ -31,8 +31,8 @@ func InitAPIHandler() http.Handler {
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"})
 	originsOk := handlers.AllowedOriginValidator(func(origin string) bool {
-		isNowSh, _ := regexp.MatchString("^https://20192lemmas-*.now.sh$", origin)
-		isLocalhost, _ := regexp.MatchString("^http://localhost*$", origin)
+		isNowSh, _ := regexp.MatchString(`^https:\/\/20192lemmas-.*\.now\.sh$`, origin)
+		isLocalhost, _ := regexp.MatchString(`^http:\/\/localhost:\d*$`, origin)
 		return isNowSh || isLocalhost
 	})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
