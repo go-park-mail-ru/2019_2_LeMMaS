0aa326a596aea1e146dd91b0e69489e4528ec92b
anton <anton.cobzew@yandex.ru>
2019-10-15 14:31:17 +0300

    Change allowed origin

--- controller/api.go
+++ controller/api.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"net/http"
+	"regexp"
 )
 
 const (
@@ -29,7 +30,11 @@ func InitAPIHandler() http.Handler {
 	router.HandleFunc(ApiV1UserLogoutPath, userController.HandleUserLogout).Methods(http.MethodPost)
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"})
-	originsOk := handlers.AllowedOriginValidator(func(origin string) bool { return true })
+	originsOk := handlers.AllowedOriginValidator(func(origin string) bool {
+		isNowSh, _ := regexp.MatchString("^https://20192lemmas-*.now.sh$", origin)
+		isLocalhost, _ := regexp.MatchString("^http://localhost*$", origin)
+		return isNowSh || isLocalhost
+	})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
 	credentials := handlers.AllowCredentials()
 	return handlers.CORS(originsOk, headersOk, methodsOk, credentials)(router)
