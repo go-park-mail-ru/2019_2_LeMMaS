fd62c162522b5c76e64abf798bffff6db91b7d06
anton <anton.cobzew@yandex.ru>
2019-10-15 14:11:51 +0300

    Allow CORS credentials

--- controller/api.go
+++ controller/api.go
@@ -31,5 +31,6 @@ func InitAPIHandler() http.Handler {
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"})
 	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000", ".now.sh"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
-	return handlers.CORS(originsOk, headersOk, methodsOk)(router)
+	credentials := handlers.AllowCredentials()
+	return handlers.CORS(originsOk, headersOk, methodsOk, credentials)(router)
 }
