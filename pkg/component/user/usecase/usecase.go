82a7cc5136afc6e9edaf1e29b9ddae9691dab4ee
anton <anton.cobzew@yandex.ru>
2019-11-16 12:02:22 +0300

    Add lebedev avatar

--- pkg/component/user/usecase/usecase.go
+++ pkg/component/user/usecase/usecase.go
@@ -72,10 +72,10 @@ func (u *userUsecase) UpdateUserAvatar(user *model.User, avatarFile io.Reader) e
 
 func (u *userUsecase) GetAvatarUrlByName(name string) string {
 	avatarsByName := map[string]string{
-		"eath":   "http://www.i2clipart.com/cliparts/3/d/1/e/clipart-earth-3d1e.png",
-		"trump":  "https://lemmas.s3.eu-west-3.amazonaws.com/trump.png",
-		"heroku": "https://railsware.com/blog/wp-content/uploads/2017/12/How-to-set-up-the-Heroku.png",
-		"cat":    "https://i.pinimg.com/originals/90/a8/56/90a856d434dd9df24d8d5fdf4bf3ce72.png",
+		"eath":    "http://www.i2clipart.com/cliparts/3/d/1/e/clipart-earth-3d1e.png",
+		"trump":   "https://lemmas.s3.eu-west-3.amazonaws.com/trump.png",
+		"lebedev": "https://lemmas.s3.eu-west-3.amazonaws.com/lebedev.jpg",
+		"cat":     "https://i.pinimg.com/originals/90/a8/56/90a856d434dd9df24d8d5fdf4bf3ce72.png",
 	}
 	return avatarsByName[strings.ToLower(name)]
 }
