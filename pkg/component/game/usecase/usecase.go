218e8d01a42314e96b9356f76facbaebefb5af48
anton <anton.cobzew@yandex.ru>
2019-11-22 16:20:39 +0300

    Set 50ms game event rate

--- pkg/component/game/usecase/usecase.go
+++ pkg/component/game/usecase/usecase.go
@@ -15,7 +15,7 @@ import (
 const (
 	maxSpeed   = 100
 	minSpeed   = 0
-	speedKoeff = float64(eventStreamRate/time.Millisecond) / 300
+	speedKoeff = float64(eventStreamRate/time.Millisecond) / 150
 
 	maxDirection = 359
 	minDirection = 0
@@ -24,7 +24,7 @@ const (
 
 	generatedFoodAmount = 10
 
-	eventStreamRate = 100 * time.Millisecond
+	eventStreamRate = 50 * time.Millisecond
 )
 
 var (
